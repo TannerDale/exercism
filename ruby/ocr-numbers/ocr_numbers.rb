55a2fb817a75d8f37b0ae5b85d82dbe138289362
Tanner Dale <tanner@tannerdale.com>
2021-05-19 18:55:29 -0500

    Refactor ocr numbers

--- ruby/ocr-numbers/ocr_numbers.rb
+++ ruby/ocr-numbers/ocr_numbers.rb
@@ -5,7 +5,9 @@ Write your code for the 'Ocr Numbers' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/ocr-numbers` directory.
 =end
-class OcrNumbers
+module OcrNumbers
+  extend self
+
   NUMBERS_USING_INDEX = {
     0 => {
       "   " => [1, 4],
@@ -26,40 +28,26 @@ class OcrNumbers
     }
   }
 
-  def self.convert(input)
-    seperated_numbers = get_all_numbers(input)
-
-    if seperated_numbers.first.length < 5
-      seperated_numbers.map { |number| decipher_number(number) }.join
-    else
-      convert_columns(seperated_numbers)
-    end
-  end
-
-  def self.get_all_numbers(input)
+  def convert(input)
     rows = input.split("\n")
 
-    raise ArgumentError if rows.any? { |row| row.length % 3 != 0 }
     raise ArgumentError if rows.length % 4 != 0
+    raise ArgumentError if rows.any? { |row| row.length % 3 != 0 }
 
-    seperate_numbers(rows)
+    rows.each_slice(4).map { |row_of_nums|
+      seperate_numbers(row_of_nums).flat_map { |num|
+        decipher_number(num)
+      }.join
+    }.join(",")
   end
 
-  def self.seperate_numbers(rows)
+  def seperate_numbers(rows)
     rows.map { |line|
       line.chars.each_slice(3).map(&:join)
     }.transpose
   end
 
-  def self.convert_columns(seperated_numbers)
-    columns = seperated_numbers.map do |number|
-      number.each_slice(4).map { |number| decipher_number(number) }
-    end
-
-    reformat_columns(columns)
-  end
-
-  def self.decipher_number(number)
+  def decipher_number(number)
     possible = number[..-2].each_with_index.map do |line, i|
       NUMBERS_USING_INDEX[i][line]
     end
@@ -67,11 +55,4 @@ class OcrNumbers
     possible.empty? ? "?" : possible
   end
 
-  def self.reformat_columns(columns)
-    columns.first.each_with_index.map { |numbers, i|
-      numbers.map { |number|
-        [number] + columns[1..].map { |column| column[i] }
-      }.join
-    }.join(",")
-  end
 end
